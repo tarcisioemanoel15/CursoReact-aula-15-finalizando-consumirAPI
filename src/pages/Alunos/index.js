a26a4d8f1ad5c19a375c30a02abc61500910686f
tarcisio <tarcisioemanoel10031@gmail.com>
2022-03-03 22:28:11 -0300

    comit

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import axios from '../../services/axios';
 import { Container } from '../../styles/GlobalStyles';
 
 export default function Alunos() {
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      console.log(response.data);
+    }
+    getData();
+  }, []);
   return (
     <Container>
       <h1>Alunos</h1>
