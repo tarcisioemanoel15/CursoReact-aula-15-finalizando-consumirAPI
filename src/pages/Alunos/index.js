e8729e4c11f6c5f1ded57153716d5a309be6ccca
tarcisio <tarcisioemanoel10031@gmail.com>
2022-03-05 15:51:43 -0300

    comits

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import axios from '../../services/axios';
+
+import { useState } from 'react/cjs/react.production.min';
 import { Container } from '../../styles/GlobalStyles';
+import axios from '../../services/axios';
 
 export default function Alunos() {
+  const [alunos, setAlunos] = useState([]);
+
   React.useEffect(() => {
     async function getData() {
       const response = await axios.get('/alunos');
-      console.log(response.data);
+      setAlunos(response.response.data);
     }
     getData();
   }, []);
+  /**/
   return (
     <Container>
       <h1>Alunos</h1>
+
+      {alunos.map((aluno) => aluno.nome)}
     </Container>
   );
 }
